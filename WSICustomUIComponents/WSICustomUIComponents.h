52275000763539210f0bf94060eb9cc0b0358209
RaJiV <rajiv.p@photon.in>
2021-04-27 12:42:24 +0530

    Initial commit

---
+++ WSICustomUIComponents/WSICustomUIComponents.h
@@ -0,0 +1,18 @@
+//
+//  WSICustomUIComponents.h
+//  WSICustomUIComponents
+//
+//  Created by Rajiv Puli on 27/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WSICustomUIComponents.
+FOUNDATION_EXPORT double WSICustomUIComponentsVersionNumber;
+
+//! Project version string for WSICustomUIComponents.
+FOUNDATION_EXPORT const unsigned char WSICustomUIComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WSICustomUIComponents/PublicHeader.h>
+
+
